27045f7341f275b7f50a0174b34420391abc53b2
Chisom <nwisuchisom@gmail.com>
2019-12-06 13:38:35 +0100

    Update AppConstant.cs

--- LagosArch/LagosArch/AppConstant.cs
+++ LagosArch/LagosArch/AppConstant.cs
@@ -3,7 +3,7 @@
     public class AppConstant
     {
         public const string BaseUrl = "https://catholicarchlag.org/";
-        public const string PublicKey = "FLWPUBK-42f8a591664b13f89744d2b7b3ab40e0-X";
-        public const string SecretKey = "FLWSECK-0f9d880638d7ac2f1ce36421781c806c-X";
+        public const string PublicKey = "my public key";
+        public const string SecretKey = "my secret";
     }
 }
